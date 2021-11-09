d8d7a3676510b1f5623d0e10a2e8b0b52dd504e1
walter palomino <jimmypalomino93@gmail.com>
2021-11-08 22:14:29 -0300

    Se creo clase Test del CandidatoServiceImplTest y se implemento el test del findAllTest

--- springBoot-servicio-test-backend/src/test/java/com/microservicio/app/test/backend/service/CandidatoServiceImplTest.java
+++ springBoot-servicio-test-backend/src/test/java/com/microservicio/app/test/backend/service/CandidatoServiceImplTest.java
@@ -1,4 +1,66 @@
+package com.microservicio.app.test.backend.service;
+
+import com.microservicio.app.test.backend.dto.CandidatoDto;
+import com.microservicio.app.test.backend.entity.Candidato;
+import com.microservicio.app.test.backend.entity.TipoDocumento;
+import com.microservicio.app.test.backend.repository.CandidatoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class CandidatoServiceImplTest {
-  
+
+    @Mock
+    private CandidatoRepository candidatoRepository;
+
+    @InjectMocks
+    private CandidatoServiceImpl candidatoService;
+
+    Candidato candidato;
+
+    @BeforeEach
+    void setUp() {
+
+        candidato = new Candidato(1l, "pepe", "perez",new TipoDocumento(1l,"DNI"),"123456789");
+    }
+
+    @Test
+    void findAllTest() {
+
+        List<Candidato> listaCandidato = new ArrayList<>();
+        listaCandidato.add(candidato);
+
+        Mockito.when(candidatoRepository.findAll()).thenReturn(listaCandidato);
+
+        List<CandidatoDto> candidatos = candidatoService.findAll();
+
+        assertEquals(1, candidatos.size());
+
+    }
+
+    @Test
+    void findByIdTest() {
+    }
+
+    @Test
+    void addCandidatoDtoTest() {
+    }
+
+    @Test
+    void updateCandidatoDtoTest() {
+    }
+
+    @Test
+    void deleteCandidatoTest() {
+    }
 }
