6483cf06a33729dde021849127b24240f5247e9d
walter palomino <jimmypalomino93@gmail.com>
2021-11-08 21:05:05 -0300

    Se elimino import de mas y se reemplazo el Getter y Setter de lombok por @Data

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/entity/Candidato.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/entity/Candidato.java
@@ -1,30 +1,19 @@
 package com.microservicio.app.test.backend.entity;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter @Setter 
+@Data
 @AllArgsConstructor @NoArgsConstructor
 @Entity
 @Table(name = "candidato")
