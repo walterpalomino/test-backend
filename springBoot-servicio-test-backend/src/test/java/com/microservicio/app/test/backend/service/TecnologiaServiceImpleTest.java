cf8b7e6408dcbaa5afd90c737129d747f1620723
walter palomino <jimmypalomino93@gmail.com>
2021-11-09 22:29:06 -0300

    Se creo la clase test de TecnologiaServiceImple

---
+++ springBoot-servicio-test-backend/src/test/java/com/microservicio/app/test/backend/service/TecnologiaServiceImpleTest.java
@@ -0,0 +1,31 @@
+package com.microservicio.app.test.backend.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TecnologiaServiceImpleTest {
+
+    @Test
+    void findAllTest() {
+    }
+
+    @Test
+    void findByNombreTest() {
+    }
+
+    @Test
+    void addTecnologiaTest() {
+    }
+
+    @Test
+    void updateTecnologiaTest() {
+    }
+
+    @Test
+    void deleteTecnologiaTest() {
+    }
+}
