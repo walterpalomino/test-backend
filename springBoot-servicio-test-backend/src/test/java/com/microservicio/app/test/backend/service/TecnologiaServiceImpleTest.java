49dcad8d5c7fbe4ab10c880d114f7a2dc8dd7f65
walter palomino <jimmypalomino93@gmail.com>
2021-11-09 22:55:47 -0300

    Se implemento el test findAllTest

--- springBoot-servicio-test-backend/src/test/java/com/microservicio/app/test/backend/service/TecnologiaServiceImpleTest.java
+++ springBoot-servicio-test-backend/src/test/java/com/microservicio/app/test/backend/service/TecnologiaServiceImpleTest.java
@@ -1,16 +1,47 @@
 package com.microservicio.app.test.backend.service;
 
+import com.microservicio.app.test.backend.dto.TecnologiaDto;
+import com.microservicio.app.test.backend.entity.Tecnologia;
+import com.microservicio.app.test.backend.repository.TecnologiaRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TecnologiaServiceImpleTest {
 
+    @Mock
+    private TecnologiaRepository tecnologiaRepository;
+
+    @InjectMocks
+    private TecnologiaServiceImple tecnologiaService;
+
+    Tecnologia tecnologia;
+
+    @BeforeEach
+    void setUp (){
+        tecnologia = new Tecnologia(1l, "java", 8);
+    }
+
     @Test
     void findAllTest() {
+        List<Tecnologia> tecnologias = new ArrayList<>();
+        tecnologias.add(tecnologia);
+
+        when(tecnologiaRepository.findAll()).thenReturn(tecnologias);
+        List<TecnologiaDto> listaTecnologia = tecnologiaService.findAll();
+
+        assertEquals(1, listaTecnologia.size());
     }
 
     @Test
