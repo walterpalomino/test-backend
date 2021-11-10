d52637fad9eed44882321e19b3ccaa715e702eca
walter palomino <jimmypalomino93@gmail.com>
2021-11-09 23:55:58 -0300

    Se reemplazo los Getter y Setter de lombo por @Data

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/entity/Tecnologia.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/entity/Tecnologia.java
@@ -10,13 +10,9 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;  
 import javax.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@Getter @Setter @ToString
+@Data
 @NoArgsConstructor @AllArgsConstructor
 @Entity
 @Table(name = "tecnologia")
