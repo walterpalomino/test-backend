830da77e816ea04d6de367e68d7b7058aa8018bd
walter palomino <jimmypalomino93@gmail.com>
2021-11-10 00:39:54 -0300

    Se declaro el metodo que busca por id de la tecnologia

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/TecnologiaService.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/TecnologiaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import com.microservicio.app.test.backend.dto.TecnologiaCrearDto;
 import com.microservicio.app.test.backend.dto.TecnologiaDto;
+import com.microservicio.app.test.backend.entity.Tecnologia;
 
 public interface TecnologiaService{
 
 	public List<TecnologiaDto> findAll();
+	public Tecnologia findById(long id);
 	public TecnologiaDto findByNombre(String nombre);
 	public TecnologiaDto addTecnologia(TecnologiaCrearDto tecnologia);
 	public TecnologiaDto updateTecnologia(Long id, TecnologiaCrearDto tecnologia);
