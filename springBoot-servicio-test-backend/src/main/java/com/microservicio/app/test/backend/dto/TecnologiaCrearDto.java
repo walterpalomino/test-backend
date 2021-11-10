7375a04f354f29643c7fc15fd49f59fd5d652445
walter palomino <jimmypalomino93@gmail.com>
2021-11-09 23:57:45 -0300

    Se reemplazo los Getter y Setter de lombo por @Data

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/dto/TecnologiaCrearDto.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/dto/TecnologiaCrearDto.java
@@ -2,13 +2,10 @@ package com.microservicio.app.test.backend.dto;
 
 import com.microservicio.app.test.backend.entity.Tecnologia;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
-@Getter @Setter
+@Data
 @AllArgsConstructor @NoArgsConstructor
 public class TecnologiaCrearDto {
 	
