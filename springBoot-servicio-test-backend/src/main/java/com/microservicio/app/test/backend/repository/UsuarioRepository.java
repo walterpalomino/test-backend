d21fb096a27a06a9e9bbba41154ddb5cfeec8dfe
walter palomino <jimmypalomino93@gmail.com>
2021-11-11 00:23:36 -0300

    Se creo el repositorio para el crud del usuario

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/repository/UsuarioRepository.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/repository/UsuarioRepository.java
@@ -1,2 +1,11 @@
-package com.microservicio.app.test.backend.repository;public interface UsuarioRepository {
+package com.microservicio.app.test.backend.repository;
+
+import com.microservicio.app.test.backend.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Usuario findByNombre(String nombre);
 }
