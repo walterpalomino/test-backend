205c7037244632ec9dc6ca2fbca57f6a4087cd70
walter palomino <jimmypalomino93@gmail.com>
2021-11-11 00:28:15 -0300

    Se creo la clase del service de usuario y implemento la funcionalidad que valida si el usuario que se intenta loguear existe

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/UserServiceImpl.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/UserServiceImpl.java
@@ -1,2 +1,35 @@
-package com.microservicio.app.test.backend.service;public class UserServiceImpl {
+package com.microservicio.app.test.backend.service;
+
+import com.microservicio.app.test.backend.entity.Usuario;
+import com.microservicio.app.test.backend.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioRepository.findByNombre(username);
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("ADMIN"));
+
+        UserDetails userDetails = new User(usuario.getNombre(), usuario.getClave(), roles);
+
+        return userDetails;
+    }
 }
