1a4a12f61ff424160d91b995e5d46fc12db3a517
walter palomino <jimmypalomino93@gmail.com>
2021-11-14 15:31:11 -0300

    Se cambio el parametro de entrada para eliminar la experiencia de un candidato

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/CandidatoExperienciaService.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/CandidatoExperienciaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.microservicio.app.test.backend.dto.CandidatoExperienciaCrearDto;
 import com.microservicio.app.test.backend.dto.CandidatoExperienciaDto;
+import com.microservicio.app.test.backend.entity.Candidato;
 
 public interface CandidatoExperienciaService {
 	
@@ -12,5 +13,5 @@ public interface CandidatoExperienciaService {
 	public CandidatoExperienciaDto addCandidatoExperiencia(CandidatoExperienciaCrearDto c);
 	public CandidatoExperienciaDto updateCandidatoExperiencia(Long id, CandidatoExperienciaCrearDto crearExperiencia);
 	public void deleteCandidatoExperiencia(Long id);
-	public void deleteCandidato(Long id);
+	public void deleteCandidato(Candidato candidato);
 }
