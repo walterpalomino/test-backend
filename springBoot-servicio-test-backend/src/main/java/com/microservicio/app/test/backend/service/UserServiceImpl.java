262877930c100b6e9f0b8a430e26b7a36cf660c4
walter palomino <jimmypalomino93@gmail.com>
2021-11-14 15:42:34 -0300

    Se optimizo la funcionalidad

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/UserServiceImpl.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.microservicio.app.test.backend.service;
 
+import com.microservicio.app.test.backend.entity.Role;
 import com.microservicio.app.test.backend.entity.Usuario;
 import com.microservicio.app.test.backend.repository.UsuarioRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -29,14 +31,19 @@ public class UserServiceImpl implements UserDetailsService {
         Usuario usuario = usuarioRepository.findByNombre(username);
 
         if(Objects.isNull(usuario)){
-            log.warn("El usuario " + username + " que se intenta loguear no esta registrado");
+            log.error("El usuario " + username + " que se intenta loguear no esta registrado");
+            throw new UsernameNotFoundException("El usuario " + username + " que se intenta loguear no esta registrado");
         }
 
-        List<GrantedAuthority> roles = new ArrayList<>();
-        roles.add(new SimpleGrantedAuthority("ADMIN"));
+        List<GrantedAuthority> roles = usuario.getRoles()
+                        .stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getNombre()))
+                        .peek(autority -> log.info("Role " + autority.getAuthority()))
+                        .collect(Collectors.toList());
 
-        UserDetails userDetails = new User(usuario.getNombre(), usuario.getClave(), roles);
+        log.info("Usuario autenticado " + username);
 
-        return userDetails;
+        return new User(usuario.getNombre(), usuario.getClave(), roles);
+
     }
 }
