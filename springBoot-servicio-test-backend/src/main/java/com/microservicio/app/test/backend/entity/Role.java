002fd630f7cd38abd9a3ff6311319b37b6ac7987
walter palomino <jimmypalomino93@gmail.com>
2021-11-14 15:12:00 -0300

    Se agrego la clase Role

--- springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/entity/Role.java
+++ springBoot-servicio-test-backend/src/main/java/com/microservicio/app/test/backend/entity/Role.java
@@ -1,2 +1,19 @@
-package com.microservicio.app.test.backend.entity;public class Role {
+package com.microservicio.app.test.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nombre;
 }
