5392e8b3c213047f6e5ec02bb9e1d5c6302906d5
walter palomino <jimmypalomino93@gmail.com>
2021-11-14 00:12:53 -0300

    Se creo la clase test del controlador tecnologia y se implemento el test para buscar todas las tecnologias

--- springBoot-servicio-test-backend/src/test/java/com/microservicio/app/test/backend/controller/TecnologiaControllerTest.java
+++ springBoot-servicio-test-backend/src/test/java/com/microservicio/app/test/backend/controller/TecnologiaControllerTest.java
@@ -1,13 +1,28 @@
 package com.microservicio.app.test.backend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservicio.app.test.backend.dto.TecnologiaCrearDto;
+import com.microservicio.app.test.backend.dto.TecnologiaDto;
 import com.microservicio.app.test.backend.service.TecnologiaService;
+import com.microservicio.app.test.backend.service.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TecnologiaController.class)
 class TecnologiaControllerTest {
@@ -18,8 +33,36 @@ class TecnologiaControllerTest {
     @MockBean
     private TecnologiaService tecnologiaService;
 
+    @MockBean
+    private UserServiceImpl userService;
+
+    ObjectMapper objectMapper;
+
+    private TecnologiaDto tecnologiaDto;
+    private TecnologiaCrearDto tecnologiaCrearDto;
+
+    private static final long NUMBER_ONE = 1l;
+    private static final String NOMBRE_TECNOLOGIA_JAVA = "java";
+
     @BeforeEach
     void setUp() {
+
+        tecnologiaDto = new TecnologiaDto(NUMBER_ONE, NOMBRE_TECNOLOGIA_JAVA,9);
+        tecnologiaCrearDto = new TecnologiaCrearDto(null, NOMBRE_TECNOLOGIA_JAVA,9);
+
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    @WithMockUser
+    void listaTecnologiaTest() throws Exception {
+
+        List<TecnologiaDto> tecnologias = new ArrayList<>();
+        when(tecnologiaService.findAll()).thenReturn(tecnologias);
+
+        mockMvc.perform(get("/api/listado").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
 }
